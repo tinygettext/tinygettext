0db94e04ec4a0d63c7926fd5eec86955afb02e63
Ingo Ruhnke <grumbel@gmail.com>
2022-05-31 15:22:42 +0200

    Update flake.nix to nixos-22.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "A minimal replacement for gettext written in C++";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
 
-    tinycmmc.url = "gitlab:grumbel/cmake-modules";
+    tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
     tinycmmc.inputs.flake-utils.follows = "flake-utils";
   };
