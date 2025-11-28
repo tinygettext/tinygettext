ddd8d9a5b9c4c4523b85cb6f722b1d624f11db14
Ingo Ruhnke <grumbel@gmail.com>
2025-11-28 14:16:16 +0100

    Fix mixup between targetPlatform/hostPlatform

--- tinygettext.nix
+++ tinygettext.nix
@@ -13,7 +13,7 @@ stdenv.mkDerivation {
   src = lib.cleanSource ./.;
 
   postFixup = ""
-  + (lib.optionalString stdenv.targetPlatform.isWindows ''
+  + (lib.optionalString stdenv.hostPlatform.isWindows ''
     # This is rather ugly, but functional. Nix has a win-dll-link.sh
     # for this, but that's currently broken:
     # https://github.com/NixOS/nixpkgs/issues/38451
